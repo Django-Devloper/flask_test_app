541efd978f6b4f607c44c8de1a1d8d0bdecf475c
deepak <djangodevloper@gmail.com>
2026-03-08 00:33:08 +0530

    docker compose setup

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Docker Flask API is running"
-    })
+    return jsonify({"message": "Flask container running"})
 
 @app.route("/hello/<name>")
 def hello(name):
-    return jsonify({
-        "message": f"Hello {name}"
-    })
+    return jsonify({"message": f"Hello {name}"})
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
